3c5a34c57c2c174a3b28c883f3ee2e4dd9a4311e
euleo11 <leonardov.silva15@gmail.com>
2023-01-16 23:20:25 -0300

    a053 typescript

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
+export enum PRODUCT_CATEGORY {
+    ACCESSORIES = "Acessórios",
+    CLOTHES_AND_SHOES = "Roupas e calçados",
+    ELECTRONICS = "Eletrônicos"
+}
+
 export type TUsers = {
     id: string,
     email: string,
